6a2073920dd3bad7def471fb29261b4eb1dbb201
Diego Pupato <pupato.diego@gmail.com>
2018-11-21 08:41:10 +1300

    Removing student test profile when run application.

--- app/src/main/java/nz/park/kenneth/wintecdm/database/Data/Students.java
+++ app/src/main/java/nz/park/kenneth/wintecdm/database/Data/Students.java
@@ -3,7 +3,7 @@ package nz.park.kenneth.wintecdm.database.Data;
 public class Students {
 
     public String[][] content = {
-            {"1000", "Paul Barber","Bachelors .IT",null,"1","paul.barber@student.wintec.ac.nz"}
+            //{"1000", "Paul Barber","Bachelors .IT",null,"1","paul.barber@student.wintec.ac.nz"}
 
     };
 
